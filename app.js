80554dc35c9f1c38bead026e4dce6a8c8670e514
Connor Peshek <Connorpeshek@gmail.com>
2016-04-18 19:28:30 -0500

    add basic structure for app

--- app.js
+++ app.js
@@ -7,6 +7,54 @@
   function updateElement() {
     element = Eager.createElement(options.location, element)
     element.classList.add(CONTAINER_CLASS)
+
+    const selfSummary = document.createElement("eager-self-summary-content")
+
+    element.appendChild(selfSummary)
+
+    const closeButton = document.createElement("eager-self-summary-content-closeButton")
+
+    selfSummary.appendChild(closeButton)
+    closeButton.addEventListener("click", hide)
+
+    const image = document.createElement("img")
+
+    image.src = options.image
+    selfSummary.appendChild(image)
+
+    const header = document.createElement("h2")
+
+    header.innerText = options.messageTitle
+    selfSummary.appendChild(header)
+
+    const message = document.createElement("eager-self-summary-content-message")
+
+    message.innerText = options.message
+    selfSummary.appendChild(message)
+
+    const email = document.createElement("eager-self-summary-content-email")
+
+    email.innerText = `Email: ${options.email}`
+    selfSummary.appendChild(email)
+
+    const twitter = document.createElement("eager-self-summary-content-twitter")
+
+    twitter.innerText = `Twitter: ${options.twitter}`
+    selfSummary.appendChild(twitter)
+
+    const facebook = document.createElement("eager-self-summary-content-facebook")
+
+    facebook.innerText = `Facebook: ${options.facebook}`
+    selfSummary.appendChild(facebook)
+
+    const linkedIn = document.createElement("eager-self-summary-content-linkedIn")
+
+    linkedIn.innerText = `LinkedIn: ${options.linkedIn}`
+    selfSummary.appendChild(linkedIn)
+
+    function hide() {
+      element.removeChild(selfSummary)
+    }
   }
 
   if (document.readyState === "loading") {
