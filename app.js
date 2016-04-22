3880a765782e4e1e805d621f049b65a63afd7271
Teffen Ellis (Eric) <teffen@nirri.us>
2016-04-21 18:02:01 -0700

    Flesh out child alignment.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
   function gravatarURL(email = "", size = 160) {
     const emailMd5 = window.EagerGravatar.md5(email.toLowerCase())
 
-    return `http://www.gravatar.com/avatar/${emailMd5}".jpg?s=${size}`
+    return `http://www.gravatar.com/avatar/${emailMd5}.jpg?s=${size}`
   }
 
   function updateElement() {
@@ -15,22 +15,34 @@
     element.classList.add(CONTAINER_CLASS)
 
     const imageSrc = options.image ? options.image : gravatarURL(options.email)
+    const wrapper = document.createElement("eager-wrapper")
+    // TODO: something like
+    // wrapper.setAttribute('data-position', options.position)
 
-    element.innerHTML += `<eager-avatar style="background-image: url(${imageSrc});"></eager-avatar>`
-    element.innerHTML += `<eager-message>${options.message.html}</eager-message>`
+    wrapper.innerHTML = `
+      <eager-details>
+        <eager-avatar style="background-image: url(${imageSrc});"></eager-avatar>
+        <eager-message>${options.message.html}</eager-message>
+      </eager-details>
+    `
 
     if (options.email) {
-      element.innerHTML += `<a class="eager-social" data-icon="email" href="mailto:${options.email}"></a>`
+      wrapper.innerHTML += `<a class="eager-social" data-icon="email" href="mailto:${options.email}"></a>`
     }
+
     if (options.twitter) {
-      element.innerHTML += `<a class="eager-social" data-icon="twitter" href="https://www.twitter.com/${options.twitter}"></a>`
+      wrapper.innerHTML += `<a class="eager-social" data-icon="twitter" href="https://www.twitter.com/${options.twitter}"></a>`
     }
+
     if (options.facebook) {
-      element.innerHTML += `<a class="eager-social" data-icon="facebook" href="https://www.facebook.com/${options.facebook}"></a>`
+      wrapper.innerHTML += `<a class="eager-social" data-icon="facebook" href="https://www.facebook.com/${options.facebook}"></a>`
     }
+
     if (options.linkedIn) {
-      element.innerHTML += `<a class="eager-social" data-icon="linkedIn" href="https://www.linkedin.com/in/${options.linkedIn}"></a>`
+      wrapper.innerHTML += `<a class="eager-social" data-icon="linkedIn" href="https://www.linkedin.com/in/${options.linkedIn}"></a>`
     }
+
+    element.appendChild(wrapper)
   }
 
   if (document.readyState === "loading") {
--- build/app.js
+++ build/app.js
@@ -12,7 +12,7 @@
 
     var emailMd5 = window.EagerGravatar.md5(email.toLowerCase());
 
-    return "http://www.gravatar.com/avatar/" + emailMd5 + "\".jpg?s=" + size;
+    return "http://www.gravatar.com/avatar/" + emailMd5 + ".jpg?s=" + size;
   }
 
   function updateElement() {
@@ -20,22 +20,29 @@
     element.classList.add(CONTAINER_CLASS);
 
     var imageSrc = options.image ? options.image : gravatarURL(options.email);
+    var wrapper = document.createElement("eager-wrapper");
+    // TODO: something like
+    // wrapper.setAttribute('data-position', options.position)
 
-    element.innerHTML += "<eager-avatar style=\"background-image: url(" + imageSrc + ");\"></eager-avatar>";
-    element.innerHTML += "<eager-message>" + options.message.html + "</eager-message>";
+    wrapper.innerHTML = "\n      <eager-details>\n        <eager-avatar style=\"background-image: url(" + imageSrc + ");\"></eager-avatar>\n        <eager-message>" + options.message.html + "</eager-message>\n      </eager-details>\n    ";
 
     if (options.email) {
-      element.innerHTML += "<a class=\"eager-social\" data-icon=\"email\" href=\"mailto:" + options.email + "\"></a>";
+      wrapper.innerHTML += "<a class=\"eager-social\" data-icon=\"email\" href=\"mailto:" + options.email + "\"></a>";
     }
+
     if (options.twitter) {
-      element.innerHTML += "<a class=\"eager-social\" data-icon=\"twitter\" href=\"https://www.twitter.com/" + options.twitter + "\"></a>";
+      wrapper.innerHTML += "<a class=\"eager-social\" data-icon=\"twitter\" href=\"https://www.twitter.com/" + options.twitter + "\"></a>";
     }
+
     if (options.facebook) {
-      element.innerHTML += "<a class=\"eager-social\" data-icon=\"facebook\" href=\"https://www.facebook.com/" + options.facebook + "\"></a>";
+      wrapper.innerHTML += "<a class=\"eager-social\" data-icon=\"facebook\" href=\"https://www.facebook.com/" + options.facebook + "\"></a>";
     }
+
     if (options.linkedIn) {
-      element.innerHTML += "<a class=\"eager-social\" data-icon=\"linkedIn\" href=\"https://www.linkedin.com/in/" + options.linkedIn + "\"></a>";
+      wrapper.innerHTML += "<a class=\"eager-social\" data-icon=\"linkedIn\" href=\"https://www.linkedin.com/in/" + options.linkedIn + "\"></a>";
     }
+
+    element.appendChild(wrapper);
   }
 
   if (document.readyState === "loading") {
